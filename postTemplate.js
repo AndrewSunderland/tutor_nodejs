64019887b29a0ba0c8d3ce498e58ddcd82b71a2c
robert-yelas-1516 <robdev@live.com>
2018-11-29 18:38:38 -0800

    Templatized eveverything, made server.js, included JSON files

---
+++ postTemplate.js
@@ -0,0 +1,28 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['postTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "  <div class=\"post\" data-name= \""
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "\" data-subject= \""
+    + alias4(((helper = (helper = helpers.subject || (depth0 != null ? depth0.subject : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"subject","hash":{},"data":data}) : helper)))
+    + "\" data-price=\""
+    + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
+    + "\" data-phone=\""
+    + alias4(((helper = (helper = helpers.phone || (depth0 != null ? depth0.phone : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"phone","hash":{},"data":data}) : helper)))
+    + "\" data-profile="
+    + alias4(((helper = (helper = helpers.profile || (depth0 != null ? depth0.profile : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"profile","hash":{},"data":data}) : helper)))
+    + ">\n    <div class=\"post-contents\">\n      <div class=\"post-image-container\">\n        <img src="
+    + alias4(((helper = (helper = helpers.photoURL || (depth0 != null ? depth0.photoURL : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"photoURL","hash":{},"data":data}) : helper)))
+    + " alt=\""
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "\">\n      </div>\n      <div class=\"post-info-container\">\n        <a href=\"#\" class=\"post-subject\">"
+    + alias4(((helper = (helper = helpers.subject || (depth0 != null ? depth0.subject : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"subject","hash":{},"data":data}) : helper)))
+    + "</a> <span class=\"post-price\">$"
+    + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
+    + "/hour</span> <span class=\"post-profile\">("
+    + alias4(((helper = (helper = helpers.profile || (depth0 != null ? depth0.profile : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"profile","hash":{},"data":data}) : helper)))
+    + ")</span>\n      </div>\n    </div>\n  </div>";
+},"useData":true});
+})();
---
+++ public/index.js
@@ -0,0 +1,61 @@
+var body = document.body;
+
+
+var modalBackdrop = document.getElementById('modal-backdrop');
+var sellButton = document.getElementById('become-button');
+
+//when someone clicks the become tutor button
+
+function handleBecomeClick(event){
+    console.log("clicked button");
+    modalBackdrop.style.display = "block";
+    modalPage.style.display = "block";
+}
+var modalPage = document.getElementById('become-tutor-modal');
+sellButton.addEventListener('click', handleBecomeClick);
+
+
+//for cancel/exit on modal
+function handleCancel(event){
+    console.log("Clicked cancel")
+    modalBackdrop.style.display = "none";
+    modalPage.style.display = "none";
+}
+
+function handleExitClick(event){
+    console.log("Clicked exit");
+    modalBackdrop.style.display = "none";
+    modalPage.style.display = "none";
+}
+
+var buttonCancel = document.getElementById('modal-cancel');
+buttonCancel.addEventListener('click', handleCancel);
+
+var exitButtonModal = document.getElementById('modal-close');
+exitButtonModal.addEventListener('click', handleExitClick);
+
+
+//Dynamic tutor adding
+
+var context = {
+    photoURL: "http://placekitten.com/320/320/",
+    profile: "kitty",
+    price: "5",
+    subject: "Math",
+    condition: "Fair"
+  };
+
+var photoContainer = document.getElementById('posts');
+
+function insertNewPost(description, photoURL, price, city, condition) {
+    var context = {
+      photoURL: photoURL,
+      description: description,
+      price: price,
+      city: city,
+      condition: condition,
+    }
+    var photoCardHTML = Handlebars.templates.postTemplate(context);
+    var photoContainer = document.getElementById('posts');
+    photoContainer.insertAdjacentHTML('beforeend', photoCardHTML);
+  }
