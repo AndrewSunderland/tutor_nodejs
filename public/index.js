2346dffbf44ce90fa4e887d9fb3809d734f95dbe
robert-yelas-1516 <robdev@live.com>
2018-11-30 16:07:05 -0800

    Add new post, trying to append to DOM, not working yet

--- public/index.js
+++ public/index.js
@@ -1,6 +1,31 @@
 
 var body = document.body;
 
+var profileTextHolder = "";
+function handleProfile(event){
+  profileTextHolder = event.target.value;
+}
+var photoUrlHolder = "";
+function handlePhotoUrl(event){
+  photoUrlHolder = event.target.value;
+}
+var priceHolder = "";
+function handlePrice(event){
+  priceHolder = event.target.value;
+}
+var subjectHolder = "";
+function handleSubject(event){
+  subjectHolder = event.target.value;
+}
+var phoneHolder = "";
+function handlePhone(event){
+  phoneHolder = event.target.value;
+}
+var nameHolder = "";
+function handleName(event){
+  nameHolder = event.target.value;
+}
+
 var modalBackdrop = document.getElementById('modal-backdrop');
 var sellButton = document.getElementById('become-button');
 
@@ -40,15 +65,52 @@ exitButtonModal.addEventListener('click', handleExitClick);
 
 var photoContainer = document.getElementById('posts');
 
-function insertNewPost(description, photoURL, price, city, condition) {
+function insertNewPost(event) {
+    if(photoUrlInput && profileInput && priceInput && subjectInput && nameInput){
     var context = {
-      photoURL: photoURL,
-      description: description,
-      price: price,
-      city: city,
-      condition: condition,
+      photoURL: photoUrlHolder,
+      profile: profileTextHolder,
+      price: priceHolder,
+      subject: subjectHolder,
+      name: nameHolder
     }
+    context.photoURL = photoUrlInput;
+    context.description = profileInput;
+    context.price = priceInput;
+    context.subject = subjectInput;
+    context.name = nameInput;
+
+
     var photoCardHTML = Handlebars.templates.postTemplate(context);
     var photoContainer = document.getElementById('posts');
     photoContainer.insertAdjacentHTML('beforeend', photoCardHTML);
+    }else{
+        alert("Please enter all of the information");
+    }
   }
+
+  
+
+  //Input holders for modal inputs
+
+//
+var subjectInput = document.getElementById('post-subject-input');
+subjectInput.addEventListener('change', handleSubject);
+
+var priceInput = document.getElementById('post-price-input');
+priceInput.addEventListener('change', handlePrice);
+
+var photoUrlInput = document.getElementById('post-url-input');
+photoUrlInput.addEventListener('change', handlePhotoUrl);
+
+var phoneInput = document.getElementById('post-phone-input');
+phoneInput.addEventListener('change', handlePhone);
+
+var nameInput = document.getElementById('post-text-input');
+nameInput.addEventListener('change', handleName);
+
+var profileInput = document.getElementById('post-description-input');
+profileInput.addEventListener('change', handleProfile);
+
+var buttonAccept = document.getElementById('modal-accept');
+buttonAccept.addEventListener('click', insertNewPost);
